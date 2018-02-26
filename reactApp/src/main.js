d2cbe627aae4b112b6236fae84b044120576b55b
alvin7285 <31263725+alvin7285@users.noreply.github.com>
2018-02-26 16:50:52 +0800

    deleted edit function

--- reactApp/src/main.js
+++ reactApp/src/main.js
@@ -18,10 +18,8 @@ function addProduct(name, description, price, category, quantity) {
    price: price,
    category: category,
    quantity: quantity,
-   completed: false
  };
 }
-function editProduct()
 
 function deleteProduct(index) {
   return {
@@ -46,7 +44,7 @@ function productApp(state, action) {
         description: action.description,
         price: action.price,
         category: action.category,
-        quantity: action.quantity,
+        quantity: action.quantity
       });
       return newState;
 
@@ -93,7 +91,7 @@ class AddProductForm extends React.Component {
       name: '',
       description: '',
       price: '',
-      category: 'food',
+      category: 'Food',
       quantity: ''
     };
     this.onNameChanged=this.onNameChanged.bind(this);
@@ -177,7 +175,7 @@ class ProductItem extends React.Component {
          <div>{this.props.name}</div>
          <div>{this.props.description}</div>
          <div>{this.props.price}</div>
-         <div style={{}}>{this.props.category.toUpperCase()}</div>
+         <div>{this.props.category}</div>
          <div>{this.props.quantity}</div>
 
         <a href="#" onClick={this.onDeleteClick.bind(this)}
